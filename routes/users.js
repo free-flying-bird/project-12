dfa13411bb539fade5c07f321f80ea4c6f5060d5
Sviatoslav Sokolov <sokolik90@yandex.ru>
2020-08-04 13:32:24 +0300

    Added an error catcher and normalized Eslint

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,27 @@
 const usersRouter = require('express').Router();
 const path = require('path');
 const fsPromises = require('fs').promises;
+
 const usersPath = path.join(__dirname, '../data/users.json');
 
 usersRouter.get('/users', (req, res) => {
-  if (!cardsPath) {
-    res.status(500).send({ message: 'Запрашиваемый ресурс не найден' })
-  };
-  res.send(require(usersPath));
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })
+    .then((data) => {
+      const users = JSON.parse(data);
+      if (!users) {
+        res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+      }
+      res.send(users);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Исходный файл не найден' });
+    });
 });
 
 function getUser(id, users) {
+  // eslint-disable-next-line no-underscore-dangle
   return users.find((el) => el._id === id);
-};
+}
 
 usersRouter.get('/users/:id', (req, res) => {
   fsPromises.readFile(usersPath, { encoding: 'utf8' })
@@ -21,10 +30,12 @@ usersRouter.get('/users/:id', (req, res) => {
       const seachedUser = getUser(req.params.id, users);
       if (!seachedUser) {
         res.status(404).send({ message: 'Нет пользователя с таким id' });
-
       }
       res.send(seachedUser);
     })
+    .catch(() => {
+      res.status(500).send({ message: 'Исходный файл не найден' });
+    });
 });
 
 module.exports = usersRouter;
